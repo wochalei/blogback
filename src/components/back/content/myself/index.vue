28ebb6574beb7820a0d392a2887fa59bd2d09b19
wochalei <1643527245@qq.com>
2022-03-31 23:38:58 +0800

    补充个人信息和修改博客时类别自动校准

--- src/components/back/content/myself/index.vue
+++ src/components/back/content/myself/index.vue
@@ -1,38 +1,39 @@
 <template>
   <div>
-  <div class="myself" v-if="load" >
-    <user :name="name" :img="img" />
-    <my-title :icon="'iconfont icon-zhexiantu'" :title="'文章统计图'" />
-    <!-- <v-md-preview :text="text" ref="preview"></v-md-preview> -->
-    <div class="charts">
-      <div class="chartsI">
-      <chart :option="optionA" :width="width" :height="height" />
+    <div class="myself" v-if="load">
+      <user :name="name" :img="img" />
+      <my-title :icon="'iconfont icon-zhexiantu'" :title="'文章统计图'" />
+      <!-- <v-md-preview :text="text" ref="preview"></v-md-preview> -->
+      <div class="charts">
+        <div class="chartsI">
+          <chart :option="optionA" :width="width" :height="height" />
+        </div>
+        <div class="chartsI">
+          <chart :option="optionB" :width="width" :height="height" />
+        </div>
+        <div class="chartsI">
+          <chart :option="optionC" :width="width" :height="height" />
+        </div>
       </div>
-      <div class="chartsI">
-      <chart :option="optionB" :width="width" :height="height" />
+      <my-title :icon="'iconfont icon-gongjuxiang'" :title="'个人技能'" />
+      <div class="skills">
+        <skill v-for="(item, index) in skills" :key="index" :item="item" />
       </div>
-      <div class="chartsI">
-      <chart :option="optionC" :width="width" :height="height" />
-      </div>
+      <my-title :icon="'iconfont icon-guidang'" :title="'个人项目'" />
+      <project/>
+      <my-title :icon="'iconfont icon-dengpao'" :title="'个人爱好'" />
+      <hobby/>
+      <my-title :icon="'iconfont icon-xiangce'" :title="'个人相册'" />
+      <album />
     </div>
-    <my-title :icon="'iconfont icon-gongjuxiang'" :title="'个人技能'" />
-    <div class="skills">
-      <skill v-for="(item, index) in skills" :key="index" :item="item" />
-    </div>
-    <my-title :icon="'iconfont icon-guidang'" :title="'个人项目'" />
-    <div class="placeholer">占位还没想好</div>
-    <my-title :icon="'iconfont icon-dengpao'" :title="'个人爱好'" />
-    <div class="placeholer">占位还没想好</div>
-    <my-title :icon="'iconfont icon-xiangce'" :title="'个人相册'" />
-    <album />
-  </div>
-  <div class="loadbox" v-else> <load /></div>
-  
+    <div class="loadbox" v-else><load /></div>
   </div>
 </template>
 <script>
-import load from '.././../../common/loading/loaddata'
+import load from ".././../../common/loading/loaddata";
 import album from "./album/index.vue";
+import project from "./project/index.vue";
+import hobby from './hobby'
 import { user_infor, blog } from "../../../../http/api";
 import chart from "../../../charts";
 import user from "./user";
@@ -62,7 +63,7 @@ export default {
         { skill: "Web安全", precentage: "30%" },
         { skill: "小程序", precentage: "40%" },
       ],
-      load:false
+      load: false,
     };
   },
   methods: {
@@ -79,47 +80,47 @@ export default {
       this.optionA.series[0].data = res;
       return map;
     },
-    getOptionB(map){
+    getOptionB(map) {
       let max = 0;
-      let indicator = [], number=[];
-     
-      for(let value of Object.values(map)){
-        max+=value;
-      } 
-   
-      for(let [key, value] of Object.entries(map)){
-         indicator.push({name:key,max,min:0});
-         number.push(value);
-      } 
+      let indicator = [],
+        number = [];
 
-      this.optionB.radar[0].indicator =indicator,
-      
-      this.optionB.series[0].data[0].value=number;
+      for (let value of Object.values(map)) {
+        max += value;
+      }
+
+      for (let [key, value] of Object.entries(map)) {
+        indicator.push({ name: key, max, min: 0 });
+        number.push(value);
+      }
+
+      (this.optionB.radar[0].indicator = indicator),
+        (this.optionB.series[0].data[0].value = number);
     },
     getOptionC() {
-      let res=new Array(7).fill(0),dayRange=[];
-      let day = new Date().getDay();      
-      let {Y,M,D} =timeToYm(Date.now());
-      day= day===0? 7:day;
-      D=D-day+1;
+      let res = new Array(7).fill(0),
+        dayRange = [];
+      let day = new Date().getDay();
+      let { Y, M, D } = timeToYm(Date.now());
+      day = day === 0 ? 7 : day;
+      D = D - day + 1;
       //根据当前星期几 生成对应一周几号的集合
-      for(let i=0;i<7;i++){
-        dayRange.push(D+i);
-      } 
+      for (let i = 0; i < 7; i++) {
+        dayRange.push(D + i);
+      }
       //然后循环数据 如果有几号的就+1
-     this.chartData.forEach(item=>{
-        if(item.Y===Y&&item.M===M){
+      this.chartData.forEach((item) => {
+        if (item.Y === Y && item.M === M) {
           let index = dayRange.indexOf(item.D);
-          if(index!=-1){
+          if (index != -1) {
             res[index]++;
           }
         }
-      })
-     this.optionC={series:[{data:res}]};
-      
+      });
+      this.optionC = { series: [{ data: res }] };
     },
     formateChartData() {
-     return blog.getAll().then((res) => {
+      return blog.getAll().then((res) => {
         this.chartData = res.data.data.map((item) => {
           let obj = { type: item.type, ...timeToYm(item.created) };
           return obj;
@@ -132,23 +133,22 @@ export default {
         /* this.text = data.introduce; */
         this.name = data.name;
         this.img = data.img;
-        this.load=true;
+        this.load = true;
       });
-    }
+    },
   },
   mounted() {
     this.getUserData();
     this.formateChartData()
-    .then(()=>{
-     return this.getOptionA();
-    })
-    .then(res=>{
-      this.getOptionB(res);
-    })
-    .then(()=>{
-       this.getOptionC();
-    });
-     
+      .then(() => {
+        return this.getOptionA();
+      })
+      .then((res) => {
+        this.getOptionB(res);
+      })
+      .then(() => {
+        this.getOptionC();
+      });
   },
   components: {
     user,
@@ -156,12 +156,14 @@ export default {
     myTitle,
     skill,
     album,
-    load
+    load,
+    project,
+    hobby
   },
 };
 </script>
 <style lang="scss">
-.loadbox{
+.loadbox {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -183,7 +185,7 @@ export default {
   width: 100%;
   grid-template-columns: repeat(3, 1fr);
   justify-items: center;
-  .chartsI{
+  .chartsI {
     width: 100%;
 
     overflow: hidden;
@@ -194,27 +196,24 @@ export default {
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 20px;
 }
-.placeholer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 25px;
-  font-weight: 600;
-  color: #7777;
-  height: 200px;
-}
-@media screen and (max-width:980px){
-    .charts,.skills,.album{
-      grid-template-columns: repeat(2,1fr);
-     
-    }
-}
-@media screen and (max-width:700px){
-    .charts,.skills,.album{
-      grid-template-columns: repeat(1,1fr);
-     
-    }
-}
 
-
+@media screen and (max-width: 980px) {
+  .charts,
+  .skills,
+  .album,
+  .project,
+  .hobby
+  {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+@media screen and (max-width: 700px) {
+  .charts,
+  .skills,
+  .album,
+  .project,
+  .hobby {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
 </style>
---
+++ src/components/back/content/myself/project/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="project">
+        <a class="project-item" href="https://github.com/wochalei/blogback" target="blank">
+             <div class="project-icon">
+             <i class="iconfont icon-wenzhang2"></i>
+             </div>
+             <p class="project-title">myBlog</p>
+             <p class="project-brief">本系统博客前端源码</p>
+        </a>
+        <a class="project-item" href="https://github.com/wochalei/blogend" target="blank">
+             <div class="project-icon">
+             <i class="iconfont icon-wenzhang2"></i>
+             </div>
+             <p class="project-title">myBlog</p>
+             <p class="project-brief">本系统博客后端源码</p>
+        </a>
+        <a class="project-item" href="https://github.com/wochalei/xiaomi" target="blank">
+             <div class="project-icon">
+             <i class="iconfont icon-wenzhang2"></i>
+             </div>
+             <p class="project-title">小米商城</p>
+             <p class="project-brief">原生js项目版</p>
+        </a>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    .project{
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        .project-item{
+            display: block;
+            padding: 30px 0;
+            transition: all 0.7s;
+          .project-icon{
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: 60px;
+              height: 60px;
+              border-radius: 50%;
+              background-color: #55efc4;
+              margin: 0 auto 30px auto;
+              i{
+                  font-size: 30px;
+                  color: honeydew;
+              }
+          }
+           .project-title{
+               color: #34495e;
+               font-size: 18.75px;
+               text-align: center;
+               margin-bottom: 15px;
+           }
+           .project-brief{
+               color:#999999;
+               font-size: 13.5px;
+               line-height: 20px;
+               text-align: center;
+           }
+           &:hover{
+               
+               background-color:rgba($color: #000000, $alpha: 0.1);
+           }
+        }
+    }
+
+</style>
